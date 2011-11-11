d24436a694763e811b9852e809ed3fd52a7f58e2
Rigidus <avenger-f@yandex.ru>
2011-11-11 16:11:33 +0400

    search by map for supplier-resources

--- searching.lisp
+++ searching.lisp
@@ -12,16 +12,39 @@
 
 
 (defmethod searching ((category (eql :supplier)) text)
-  (mapcar #'(lambda (x)
-              (format nil "<a href=\"/supplier/~A\">~A</a><br/><br/>"
-                      (car x)
-                      (a-name (cdr x))))
-          (remove-if-not #'(lambda (x)
-                             (and
-                              (equal 'supplier (type-of (cdr x)))
-                              (search text (string-downcase (string-trim '(#\Space #\Tab #\Newline) (a-name (cdr x)))))
-                              ))
-                         (cons-hash-list *USER*))))
+  (format nil "<br/><br/>~{~A~}"
+          (mapcar #'(lambda (x)
+                      (format nil "<a href=\"/supplier/~A\">~A</a><br/><br/>"
+                              (car x)
+                              (a-name (cdr x))))
+                  (remove-if-not #'(lambda (x)
+                                     (and
+                                      (equal 'supplier (type-of (cdr x)))
+                                      (search text (string-downcase (string-trim '(#\Space #\Tab #\Newline) (a-name (cdr x)))))
+                                      ))
+                                 (cons-hash-list *USER*)))))
+
+
+(defmethod searching ((category (eql :map)) text)
+  (let ((results)
+        (points))
+    (loop :for (id . supplier) :in (remove-if-not #'(lambda (x) (equal 'supplier (type-of (cdr x)))) (cons-hash-list *user*)) :collect
+       (loop :for supplier-resource :in (a-resources supplier) :collect
+          (if (not (null (search text (string-downcase (string-trim '(#\Space #\Tab #\Newline) (a-name (a-resource supplier-resource)))))))
+              (progn
+                (push supplier results)
+                (return)))))
+    (setf points
+          (mapcar #'(lambda (x)
+                      (mi 'yapoint
+                          :title (a-name x)
+                          :descr (a-actual-address x)
+                          :coord (geo-coder (a-actual-address x))))
+                  results))
+    (show-block
+     (mi 'yamap
+         :center-coord "30.313622, 59.937720"
+         :mark-points points))))
 
 
 (restas:define-route search-page/post ("/search" :method :post)
@@ -36,9 +59,10 @@
                    (cond ((= 0 (length text)) "Задан пустой поисковый запрос")
                          ((> 3 (length text)) "Слишком короткий поисковый запрос")
                          (t  (let ((results   (cond  ((string= "supplier" category) (searching :supplier text))
-                                                      ((string= "all" category)      (concatenate 'list
-                                                                                                  (searching :supplier text)))
-                                                      (t (format nil "~A" (bprint category))))))
-                                (if (null results)
-                                    "Ничего не найдено"
-                                    (format nil "<br/><br/>~{~A~}" results))))))))))))
+                                                     ((string= "map"      category) (searching :map text))
+                                                     ((string= "all" category)      (concatenate 'list
+                                                                                                 (searching :supplier text)))
+                                                     (t (format nil "~A" (bprint category))))))
+                               (if (null results)
+                                   "Ничего не найдено"
+                                   results)))))))))))
